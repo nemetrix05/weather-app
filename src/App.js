8cc12d3dca10995e625eae7346eb74791941c0bd
Gilmar Andres Cruz Castro <gilmar.cruz@globant.com>
2018-12-17 16:19:57 -0500

    mas avances cursos

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import LocationList from './components/LocationList';
 
+const cities = [
+  'Bogotá,col',
+  'Buenos Aires,ar',
+  'Cancún,mex',
+  'Rio de Janeiro,bra',
+  'Ciudad de Panamá,pan'
+];
+
 class App extends Component {
   render() {
     return (
       <div className="App">
           <div className="leftpanel">
-            <LocationList />
+            <LocationList cities={cities}/>
           </div>
       </div>
     );
--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import WeatherLocation from './WeatherLocation';
+// Validamos la lista de ciudades con proptipes
+import PropTypes from 'prop-types';
 
-const LocationList = () => (
-    <div>
-        <WeatherLocation city='Moscu,urss'/>
-        <WeatherLocation city='New York,eeuu'/>
-    </div>
+// Se crea una funcion que se encargara de iterar sobre cada uno de los elementos de la array
+
+const transformCity = (cities) => (
+    cities.map( (city) => <WeatherLocation city={city} key={city}/> )
 );
+
+const LocationList = ({ cities }) => {
+    return (
+        <div>
+            {transformCity(cities)}
+        </div>
+    );
+}
+
+// El parametro MAP() itera sobre los elementos de una ARRAY esuna caracteristica nueva de ES6
+
+LocationList.propTypes = {
+    cities: PropTypes.array.isRequired
+}
 
 export default LocationList;
 
